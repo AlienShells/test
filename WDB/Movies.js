4b4cbd3ccc8f960a41232771f0bbf29ff3020ed8
AlienShells <miss.maer@gmail.com>
2020-09-18 11:47:44 +0300

    исправила ошибки

--- WDB/Movies.js
+++ WDB/Movies.js
@@ -20,3 +20,30 @@ let movies = [
 		isWatched: false
 	}
 ];
+
+/* movies.forEach(function(movie) {
+	let result = 'You have ';
+	if (movie.isWatched) {
+		result += 'watched ';
+	} else {
+		result += 'not seen ';
+	}
+	result += '"' + movie.title + '" ';
+	result += movie.rating + ' stars';
+	console.log(result);
+}); */
+
+function buildString(movie) {
+	let result = 'You have ';
+	if (movie.isWatched) {
+		result += 'watched ';
+	} else {
+		result += 'not seen ';
+	}
+	result += '"' + movie.title + '" ';
+	result += movie.rating + ' stars';
+	return result;
+}
+movies.forEach(function(movie) {
+	console.log(buildString(movie));
+});
