e8ebe78fee80e91c3f29fe8a960e86cc447cad22
AlienShells <miss.maer@gmail.com>
2020-09-18 10:38:35 +0300

    Список фильмов

--- WDB/Movies.js
+++ WDB/Movies.js
@@ -1,3 +1,22 @@
-let movies{
-    
-}
+let movies = [
+	{
+		name: 'In Brudges',
+		rating: 5,
+		isWatched: true
+	},
+	{
+		name: 'Frozen',
+		rating: 4.5,
+		isWatched: false
+	},
+	{
+		name: 'Mad Max',
+		rating: 5,
+		isWatched: true
+	},
+	{
+		name: 'Les Miserables',
+		rating: 3.5,
+		isWatched: false
+	}
+];
